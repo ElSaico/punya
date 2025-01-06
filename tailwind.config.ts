276177daca449bc2f1a58df3c1db09e60919f2a2
Bruno Marques <bruno@bmarques.net>
2025-01-06 01:11:09 +0000

    Changed theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,15 @@
 import type { Config } from 'tailwindcss';
+import forms from '@tailwindcss/forms';
 import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import { concord } from '@skeletonlabs/skeleton/themes';
+import { rocket } from '@skeletonlabs/skeleton/themes';
 
 export default {
 	darkMode: 'selector',
 	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
 	plugins: [
+		forms,
 		skeleton({
-			themes: [concord]
+			themes: [rocket]
 		})
 	]
 } satisfies Config;
