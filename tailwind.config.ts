afb242ba4fa564a3110b946948246b1580e2f827
Bruno Marques <bruno@bmarques.net>
2025-02-05 21:56:04 +0000

    Remove Skeleton remnants

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,7 @@
 import type { Config } from 'tailwindcss';
-import forms from '@tailwindcss/forms';
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import { rocket } from '@skeletonlabs/skeleton/themes';
 
 export default {
 	darkMode: 'selector',
-	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
-	plugins: [
-		forms,
-		skeleton({
-			themes: [rocket]
-		})
-	]
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	plugins: []
 } satisfies Config;
