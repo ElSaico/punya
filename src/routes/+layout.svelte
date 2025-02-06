b58b2d058f7aa1c8cdeb3939b2b5e82132a51272
Bruno Marques <bruno@bmarques.net>
2025-02-06 01:18:52 +0000

    Added options to app bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+
 	import '../app.css';
 
 	const { children } = $props();
@@ -17,11 +20,50 @@
 		'Yuri Grom',
 		'Zemina Torval'
 	];
+	const apps = [
+		['combat', 'Combat'],
+		['market-flood', 'Market flood'],
+		['megaships', 'Megaships'],
+		['mining', 'Mining'],
+		['missions', 'Missions'],
+		['search-rescue', 'Search and Rescue'],
+		['settlements', 'Settlements']
+	];
+
+	function toggleAppUrl(app: string) {
+		const url = new URL(page.url);
+		const visible = (url.searchParams.get(app) == 'false').toString();
+		url.searchParams.set(app, visible);
+		goto(url, { replaceState: false });
+	}
 </script>
 
 <div class="grid h-screen grid-rows-[auto_1fr_auto]">
 	<div class="navbar bg-base-100 shadow-sm">
-		<a class="btn btn-ghost text-xl">daisyUI</a>
+		<div class="navbar-start">
+			<span class="btn btn-ghost text-xl">Punya</span>
+		</div>
+		<div class="navbar-center">
+			<select class="select">
+				<option disabled selected>Select your power</option>
+				{#each powers as power}
+					<option>{power}</option>
+				{/each}
+			</select>
+		</div>
+		<div class="navbar-end join">
+			{#each apps as [app, label]}
+				<button
+					class={{
+						'btn btn-primary join-item': true,
+						'btn-active': page.url.searchParams.get(app) === 'true'
+					}}
+					onclick={() => toggleAppUrl(app)}
+				>
+					{label}
+				</button>
+			{/each}
+		</div>
 	</div>
 	<main class="space-y-4 p-4">
 		{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	import Combat from '$lib/components/combat.svelte';
+	import MarketFlood from '$lib/components/market-flood.svelte';
+	import Megaships from '$lib/components/megaships.svelte';
+	import Mining from '$lib/components/mining.svelte';
+	import Missions from '$lib/components/missions.svelte';
+	import SearchRescue from '$lib/components/search-rescue.svelte';
+	import Settlements from '$lib/components/settlements.svelte';
+
+	function isAppActive(app: string) {
+		return page.url.searchParams.get(app) === 'true';
+	}
+</script>
+
+{#if isAppActive('combat')}<Combat />{/if}
+{#if isAppActive('market-flood')}<MarketFlood />{/if}
+{#if isAppActive('megaships')}<Megaships />{/if}
+{#if isAppActive('mining')}<Mining />{/if}
+{#if isAppActive('missions')}<Missions />{/if}
+{#if isAppActive('search-rescue')}<SearchRescue />{/if}
+{#if isAppActive('settlements')}<Settlements />{/if}
