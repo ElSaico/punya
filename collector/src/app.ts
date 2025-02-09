c7e7d1f7819ca8816926b19526df7ac62e8f0260
Bruno Marques <bruno@bmarques.net>
2025-02-09 18:04:52 +0000

    Communication between collector and worker is now done in batches

--- collector/src/app.ts
+++ collector/src/app.ts
@@ -5,51 +5,62 @@ import zlib from 'zlib';
 import type { FSDJump, FSSSignalDiscovered, Journal } from './eddn';
 
 const EDDI_URL = 'tcp://eddn.edcd.io:9500';
+const BATCH_SIZE_MEGASHIPS = 20;
+const BATCH_SIZE_SYSTEMS = 100;
 
 const eddi = new zeromq.Subscriber();
+const megaships = [];
+const systems = [];
 
 eddi.connect(EDDI_URL);
 eddi.subscribe('');
 console.log('EDDI listener connected successfully');
 
+async function request(endpoint: string, data: object[]) {
+	const body = JSON.stringify(data);
+	await fetch(`${process.env.DATABASE_API_URL}/${endpoint}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${process.env.DATABASE_API_KEY}`
+		},
+		body
+	}).then(async (response) => {
+		if (!response.ok) {
+			console.error(`[megaships] Status error: ${response.status}`);
+			console.error(`[megaships] Input: ${body}`);
+			console.error(`[megaships] Output: ${await response.text()}`);
+		}
+	});
+}
+
 for await (const [src] of eddi) {
 	const event: Journal = JSON.parse(zlib.inflateSync(src).toString());
 
 	// TODO drop legacy messages
-	// TODO batch stuff
 	switch (event.message.event) {
 		case 'FSSSignalDiscovered': {
 			const fss = event.message as FSSSignalDiscovered;
-			const megaships = fss.signals
+			const signals = fss.signals
 				.filter((signal) => signal.SignalType === 'Megaship')
 				.map((signal) => signal.SignalName);
 
-			if (megaships.length > 0) {
-				const data = JSON.stringify({
+			if (signals.length > 0) {
+				megaships.push({
 					timestamp: fss.timestamp,
 					systemId: fss.SystemAddress,
-					megaships
+					megaships: signals
 				});
-				await fetch(`${process.env.DATABASE_API_URL}/megaships`, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						Authorization: `Bearer ${process.env.DATABASE_API_KEY}`
-					},
-					body: data
-				}).then(async (response) => {
-					if (!response.ok) {
-						console.error(`[megaships] Status error: ${response.status}`);
-						console.error(`[megaships] Input: ${data}`);
-						console.error(`[megaships] Output: ${await response.text()}`);
-					}
-				});
+				if (megaships.length >= BATCH_SIZE_MEGASHIPS) {
+					await request('megaships', megaships);
+					megaships.splice(0);
+				}
 			}
 			break;
 		}
 		case 'FSDJump': {
 			const fsd = event.message as FSDJump;
-			const data = JSON.stringify({
+			systems.push({
 				id64: fsd.SystemAddress,
 				name: fsd.StarSystem,
 				x: fsd.StarPos[0],
@@ -57,20 +68,10 @@ for await (const [src] of eddi) {
 				z: fsd.StarPos[2],
 				power: fsd.ControllingPower
 			});
-			await fetch(`${process.env.DATABASE_API_URL}/systems`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${process.env.DATABASE_API_KEY}`
-				},
-				body: data
-			}).then(async (response) => {
-				if (!response.ok) {
-					console.error(`[systems] Status error: ${response.status}`);
-					console.error(`[systems] Input: ${data}`);
-					console.error(`[systems] Output: ${await response.text()}`);
-				}
-			});
+			if (systems.length >= BATCH_SIZE_SYSTEMS) {
+				await request('systems', systems);
+				systems.splice(0);
+			}
 			break;
 		}
 	}
--- database/src/index.ts
+++ database/src/index.ts
@@ -1,5 +1,6 @@
 import { sql } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/d1';
+import { getTableConfig } from 'drizzle-orm/sqlite-core';
 import { Hono } from 'hono';
 import { bearerAuth } from 'hono/bearer-auth';
 import { createMiddleware } from 'hono/factory';
@@ -22,8 +23,11 @@ type Bindings = {
 	API_KEY: string;
 };
 
+// {codename} {class}-class {category}
 const RE_SHIP_NEW = /^([A-Z\-.'+\d\s]+) ([A-Z][a-z]+)-class ([A-Z][a-z]+)$/;
+// {class} Class {category} {codename}
 const RE_SHIP_OLD = /^([A-Z][a-z]+) Class ([A-Za-z\s]+) ([A-Z]+-\d+)$/;
+// old type -> new type
 const MEGASHIP_CATEGORY_REMAP = new Map<string, string>([
 	['Agricultural Vessel', 'Cropper'],
 	['Bulk Cargo Ship', 'Hauler'],
@@ -33,6 +37,8 @@ const MEGASHIP_CATEGORY_REMAP = new Map<string, string>([
 	['Tanker', 'Tanker'],
 	['Tourist Ship', 'Traveller']
 ]);
+const MEGASHIP_CHUNK_SIZE = Math.floor(100 / getTableConfig(megaships).columns.length);
+const SYSTEM_CHUNK_SIZE = Math.floor(100 / getTableConfig(systems).columns.length);
 
 const PowerEnum = z.enum([
 	'A. Lavigny-Duval',
@@ -49,42 +55,48 @@ const PowerEnum = z.enum([
 	'Zemina Torval'
 ]);
 
-const SystemCreate = z.object({
-	id64: z.number().int().positive(),
-	name: z.string(),
-	x: z.number(),
-	y: z.number(),
-	z: z.number(),
-	power: PowerEnum.optional()
-});
+const SystemCreate = z
+	.object({
+		id64: z.number().int().positive(),
+		name: z.string(),
+		x: z.number(),
+		y: z.number(),
+		z: z.number(),
+		power: PowerEnum.optional()
+	})
+	.array()
+	.nonempty();
 
-const MegashipInput = z.object({
-	timestamp: z.coerce.date(),
-	systemId: z.number().int().positive(),
-	megaships: z.string().array()
-});
+const MegashipInput = z
+	.object({
+		timestamp: z.coerce.date(),
+		systemId: z.number().int().positive(),
+		megaships: z.string().array().nonempty()
+	})
+	.array()
+	.nonempty();
 
 const MegashipCreate = MegashipInput.transform((input) =>
-	input.megaships.map((name) => {
-		let _, category, shipClass, codename;
-		if (RE_SHIP_NEW.test(name)) {
-			// {codename} {class}-class {category}
-			[_, codename, shipClass, category] = name.match(RE_SHIP_NEW)!;
-			codename = codename.trim();
-		} else if (RE_SHIP_OLD.test(name)) {
-			// {class} Class {category} {codename}
-			[_, shipClass, category, codename] = name.match(RE_SHIP_OLD)!;
-			category = MEGASHIP_CATEGORY_REMAP.get(category.trim());
-		}
-		return {
-			name,
-			category,
-			shipClass,
-			codename,
-			timestamp: input.timestamp,
-			systemId: input.systemId
-		};
-	})
+	input.flatMap((signals) =>
+		signals.megaships.map((name) => {
+			let _, category, shipClass, codename;
+			if (RE_SHIP_NEW.test(name)) {
+				[_, codename, shipClass, category] = name.match(RE_SHIP_NEW)!;
+				codename = codename.trim();
+			} else if (RE_SHIP_OLD.test(name)) {
+				[_, shipClass, category, codename] = name.match(RE_SHIP_OLD)!;
+				category = MEGASHIP_CATEGORY_REMAP.get(category.trim());
+			}
+			return {
+				name,
+				category,
+				shipClass,
+				codename,
+				timestamp: signals.timestamp,
+				systemId: signals.systemId
+			};
+		})
+	)
 );
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -92,6 +104,32 @@ const apiKey = createMiddleware<{ Bindings: Bindings }>(async (c, next) => {
 	const auth = bearerAuth({ token: c.env.API_KEY });
 	return auth(c, next);
 });
+
+/*
+	D1 has a hard limit on bound parameters per query[1], thus we need to split our inserts in chunks
+	as long as drizzle lacks a high-level abstraction for that[2]
+
+	[1] https://developers.cloudflare.com/d1/platform/limits/
+
+	[2] https://github.com/drizzle-team/drizzle-orm/issues/2479
+ */
+function chunkify<T>(data: T[], size: number) {
+	return [...Array(Math.ceil(data.length / size))].map(() => data.splice(0, size));
+}
+
+/*
+	a necessary kludge to make the type checker happy, as db.batch() expects a non-empty array[1]
+
+	zod could enforce this up to the chunking stage, with .transform(), .coerce and .nonempty(),
+	but that information would be lost after building the queries with Array.map() anyway[2]
+
+	[1] https://github.com/drizzle-team/drizzle-orm/issues/1292
+
+	[2] https://github.com/microsoft/TypeScript/issues/29841
+ */
+function isTuple<T>(array: T[]): array is [T, ...T[]] {
+	return array.length > 0;
+}
 
 app.use('*', logger());
 
@@ -106,41 +144,51 @@ app.post('/systems', apiKey, zValidator('json', SystemCreate), async (c) => {
 	const db = drizzle(c.env.DB, { casing: 'snake_case' });
 	const data = c.req.valid('json');
 
-	// TODO bulk stuff
-	await db
-		.insert(systems)
-		.values(data)
-		.onConflictDoUpdate({
-			target: systems.id64,
-			set: { power: sql`excluded.power` },
-			setWhere: sql`power <> excluded.power`
-		});
+	const chunks = chunkify(data, SYSTEM_CHUNK_SIZE).map((chunk) =>
+		db
+			.insert(systems)
+			.values(chunk)
+			.onConflictDoUpdate({
+				target: systems.id64,
+				set: { power: sql`excluded.power` },
+				setWhere: sql`power <> excluded.power`
+			})
+	);
+	if (!isTuple(chunks)) {
+		return Response.json('this should be impossible', { status: 500 });
+	}
+	await db.batch(chunks);
 	return Response.json('OK');
 });
 
 app.post('/megaships', apiKey, zValidator('json', MegashipInput), async (c) => {
 	const db = drizzle(c.env.DB, { casing: 'snake_case' });
-	const data = c.req.valid('json');
+	const data = MegashipCreate.parse(c.req.valid('json'));
 
-	// TODO bulk stuff
-	const moved = await db
-		.insert(megaships)
-		.values(MegashipCreate.parse(data))
-		.onConflictDoUpdate({
-			target: megaships.name,
-			set: { systemId: sql`excluded.system_id` },
-			setWhere: sql`system_id <> excluded.system_id AND timestamp > excluded.timestamp`
-		})
-		.returning({ name: megaships.name });
-	if (moved.length > 0) {
-		await db.insert(megashipRoutes).values(
-			moved.map((entry) => ({
-				name: entry.name,
-				systemId: data.systemId,
-				timestamp: data.timestamp
-			}))
-		);
+	const chunks = chunkify(data, MEGASHIP_CHUNK_SIZE).map((chunk) =>
+		db
+			.insert(megaships)
+			.values(chunk)
+			.onConflictDoUpdate({
+				target: megaships.name,
+				set: { systemId: sql`excluded.system_id` },
+				setWhere: sql`system_id <> excluded.system_id AND timestamp > excluded.timestamp`
+			})
+			.returning()
+	);
+	if (!isTuple(chunks)) {
+		return Response.json('this should be impossible', { status: 500 });
 	}
+	const moved = await db.batch(chunks);
+
+	const chunksMoved = moved
+		.filter((chunk) => chunk.length > 0)
+		.map((chunk) => db.insert(megashipRoutes).values(chunk));
+	if (!isTuple(chunksMoved)) {
+		return Response.json('this should be impossible', { status: 500 });
+	}
+	await db.batch(chunksMoved);
+
 	return Response.json('OK');
 });
 
