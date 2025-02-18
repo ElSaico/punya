9cc1ce6d8a89702f9f4ebde84d6dc6e7e65e1a99
Bruno Marques <bruno@bmarques.net>
2025-02-18 15:31:55 +0000

    Changed power button styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	// TODO add welcome message
-
+	// Tailwind v4 has no present alternative to safelist, so all classes must be explicitly named
 	const powers = [
 		{ img: 'aisling.jpg', class: 'border-aisling text-aisling', name: 'Aisling Duval' },
 		{ img: 'delaine.jpg', class: 'border-delaine text-delaine', name: 'Archon Delaine' },
@@ -11,7 +10,7 @@
 			option: 'A. Lavigny-Duval'
 		},
 		{ img: 'patreus.jpg', class: 'border-patreus text-patreus', name: 'Denton Patreus' },
-		{ img: 'mahon.jpg', class: 'border-aisling text-aisling', name: 'Edmund Mahon' },
+		{ img: 'mahon.jpg', class: 'border-mahon text-mahon', name: 'Edmund Mahon' },
 		{ img: 'winters.jpg', class: 'border-winters text-winters', name: 'Felicia Winters' },
 		{ img: 'archer.jpg', class: 'border-archer text-archer', name: 'Jerome Archer' },
 		{ img: 'li.jpg', class: 'border-li text-li', name: 'Li Yong\u{2011}Rui' },
@@ -20,13 +19,19 @@
 		{ img: 'grom.jpg', class: 'border-grom text-grom', name: 'Yuri Grom' },
 		{ img: 'torval.jpg', class: 'border-torval text-torval', name: 'Zemina Torval' }
 	];
+	// TODO store selected power (and assign opacity-100 to its respective button)
 </script>
 
 <div class="flex h-fit p-4">
 	<div class="grid flex-1 grid-cols-3 gap-4 font-mono text-xl">
 		{#each powers as power (power.img)}
-			<button class={['flex cursor-pointer gap-4 rounded-lg border-2 p-2 text-left', power.class]}>
-				<img src="/images/{power.img}" alt={power.name} class="w-16" />
+			<button
+				class={[
+					'inset-shadow-pp flex cursor-pointer gap-4 rounded-xl border p-2 text-left opacity-50 hover:opacity-100',
+					power.class
+				]}
+			>
+				<img src="/images/{power.img}" alt={power.name} class="size-16" />
 				<span class="my-auto [word-spacing:100vw]">{power.name}</span>
 			</button>
 		{/each}
