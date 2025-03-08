5678ab22dfc347f21c2c6e95ff909aa090542723
Bruno Marques <bruno@bmarques.net>
2025-03-08 17:51:02 +0000

    Removed database worker (now on punya-api)

--- database/drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-	out: './migrations',
-	schema: './src/schema.ts',
-	casing: 'snake_case',
-	dialect: 'sqlite',
-	driver: 'd1-http'
-});
